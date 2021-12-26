479f6568854cc84a4bf6b7073ae89117918998e0
natalia sanchez <nataliasanchezmkt@gmail.com>
2021-12-26 17:52:50 -0300

    Ejercicio 1 y 2

---
+++ src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hello = (props) => {
+    return(
+        <section>
+            <h1>Hello world {props.saludo}</h1>
+        </section>
+    )
+};
+export default Hello
